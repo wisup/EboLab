2b7256d4ad808380d685acd811b6879eb0d9a8a3
Guillaume <f.e.a.rcombat@hotmail.fr>
2014-10-29 23:15:33 -0400

    up

---
+++ tile.js
@@ -0,0 +1,44 @@
+//VAR LIST----------
+//x
+//y
+//value
+//previousPosition
+//mergedFrom
+//
+//FUNCTION LIST-----------
+//Tile
+//
+//savePosition         : Put in previousPosition the value of the actual position
+//updatePosition       : Update the actual position
+//serialize            : Return the tile as an object
+
+function Tile(position, value) {
+    this.x = position.x;
+    this.y = position.y;
+    this.value = value;
+
+    this.previousPosition = null;
+    this.mergedFrom = null; // Tracks tiles that merged together
+}
+
+Tile.prototype.savePosition = function() {
+    this.previousPosition = {
+        x: this.x,
+        y: this.y
+    };
+};
+
+Tile.prototype.updatePosition = function(position) {
+    this.x = position.x;
+    this.y = position.y;
+};
+
+Tile.prototype.serialize = function() {
+    return {
+        position: {
+            x: this.x,
+            y: this.y
+        },
+        value: this.value
+    };
+};
